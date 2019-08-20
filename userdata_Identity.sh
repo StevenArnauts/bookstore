fec51f89f428cc2ef13f496d0c52402f05943d47
fwillaerlpu-at-634087270915 <frederik.willaert@gmail.com>
2019-08-21 00:43:33 +0200

    Finetuning userdata for IDS

--- userdata_Identity.sh
+++ userdata_Identity.sh
@@ -2,6 +2,8 @@
 echo "STARTING USERDATA!"
 echo "DON'T FORGET TO ADJUST THE VALUE OF CHANGEME"
 
+# Note: SSL is assumed to be handled by an ALB in front of this instance (or this collection of instances).
+
 # Register the Microsoft key, register the product repository, and install required dependencies
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
@@ -25,11 +27,11 @@ sudo git clone --single-branch --branch freetemp https://github.com/StevenArnaut
 sudo chown -R ubuntu /var/bookstore
 
 # Build and run some code (bookstore/Identiy project, in this case)
+export DOTNET_CLI_HOME=/tmp
 export ASPNETCORE_ENVIRONMENT=dev
+export ASPNETCORE_URLS=https://*:6103 # TODO: This probably isn't necessary any more - ALB can route any port to any port.
 export ConnectionStrings__identity="Host=bookstoredbtest4.c7xajvaahzcs.us-east-1.rds.amazonaws.com;Database=identity;Username=pencil42;Password=CHANGEME"
 cd bookstore/Identity/
 dotnet restore
 dotnet build
-dotnet run
-
-# Note: SSL is assumed to be handled by an ALB in front of this instance (or this collection of instances).
+nohup dotnet run > dotnetrun_output.txt 2>&1 &
